c1ff2ce188e57d2dea2421dff1030b1910fcb88a
Christopher Fernandez B <christfb10@gmail.com>
2023-12-12 23:37:49 -0600

    Primer COmmit

---
+++ src/main/java/com/componentes/cpsdesktop/CpsDesktop.java
@@ -0,0 +1,11 @@
+package com.componentes.cpsdesktop;
+
+import com.componentes.ui.LoginFrame;
+
+public class CpsDesktop {
+
+    public static void main(String[] args) {
+        LoginFrame loginFrame = new LoginFrame();
+        loginFrame.setVisible(true);
+    }
+}
